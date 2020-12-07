d46514c25b1a3caf7f478ba0889f77b89c51a6af
sanaz <sanazhas@kth.se>
2020-12-07 15:40:15 +0100

    ...

--- src/js/Character.js
+++ src/js/Character.js
@@ -4,14 +4,20 @@ import React from 'react';
 //const imageUrl = 'https://superheroapi.com/api/1741527979362433/70/image';
 
 const Character = ({ name, image }) => (
-    <div className="character">
-        <img src={image.url ? image.url :
-            'https://se.depositphotos.com/28850541/stock-illustration-male-default-profile-picture.html'} alt={name} />
-        <div className="character-info">
 
-            <h2>{name}</h2>
-        </div>
+    <div className="character">
+        <button type="button" class="character-btn">
+            <img src={
+                image.url ? image.url :
+                    'https://se.depositphotos.com/28850541/stock-illustration-male-default-profile-picture.html'} alt={name} />
+            <div className="character-info">
+
+                <h2>{name}</h2>
+
+            </div>
+        //</button>
     </div>
+
 
 );
 export default Character;
