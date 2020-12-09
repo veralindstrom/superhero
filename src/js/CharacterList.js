c4775872721690ad91eb38cdf28d79271fdc7d5c
sanaz <sanazhas@kth.se>
2020-12-09 23:45:34 +0100

    buttons added

--- src/js/CharacterList.js
+++ src/js/CharacterList.js
@@ -1,64 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import Character from './Character';
 
-const CharacterList = () =>{
-    //const API_KEY = '1741527979362433';
-    const featureUrl = 'https://www.superheroapi.com/api.php/1741527979362433/search/a';
-    const searchUrl = 'https://www.superheroapi.com/api.php/1741527979362433/search/';
 
-    const [characters, setCharacters] = useState([]);
-    const [searchVal, setSearchVal]= useState("");
-  
-    const getCharacters =(featureUrl)=> {
-      fetch(featureUrl)
-        .then((res) => res.json())
-        .then((data) => {
-          setCharacters(data.results);
-        });
-  
-    };
-  
-    useEffect(() => {
-      getCharacters(featureUrl);
-  
-    }, []);
+const CharacterList = () => {
 
-    const handleOnSubmit= (e) => {
-        e.preventDefault();
-        if(searchVal){
-        fetch(searchUrl + searchVal)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            setCharacters(data.results);
-          });
-          setSearchVal("");
-        }
-      };
-    
-      const handleOnChange= (e) => {
-        setSearchVal(e.target.value);
-      };
-  
-    return (
-        <>
-            <header>
-              <form onSubmit={handleOnSubmit}>
-            <input 
+  //const API_KEY = '1741527979362433';
+  const featureUrl = 'https://www.superheroapi.com/api.php/1741527979362433/search/a';
+  const searchUrl = 'https://www.superheroapi.com/api.php/1741527979362433/search/';
+
+  const [characters, setCharacters] = useState([]);
+  const [searchVal, setSearchVal] = useState("");
+
+  const getCharacters = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setCharacters(data.results);
+      });
+
+  };
+  useEffect(() => {
+    getCharacters(featureUrl);
+    //getCharacters(searchVal);
+  }, [searchVal]);
+
+
+
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchVal) {
+      getCharacters(searchUrl + searchVal);
+
+      setSearchVal("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchVal(e.target.value);
+  };
+
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
             className="search"
-            type="search" 
+            type="search"
             placeholder="Search..."
             value={searchVal}
-            onChange={handleOnChange}/>
-            </form>
-          </header>
-            <div className="character-container">
-              {characters.length > 0 &&
-                characters.map((character) =>
-                  <Character key={character.id} {...character} />
-                )}
-            </div>
-        </>);
-}
+            onChange={handleOnChange} />
+        </form>
+      </header>
+      <div className="character-container">
+        {characters.length > 0 &&
+          characters.map((character) =>
+            <Character key={character.id} {...character} />
+          )}
+      </div>
+    </>);
+
+};
 
 export default CharacterList;
