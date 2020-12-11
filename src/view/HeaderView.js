b5615a7cbde4f6c8d3254625cb861f3419b878e4
estelleh <estelleh@kth.se>
2020-12-11 21:54:44 +0100

    Updated CSS on header + mobileview, and loading gif

--- src/view/HeaderView.js
+++ src/view/HeaderView.js
@@ -8,24 +8,22 @@ const HeaderView = () =>{
                 <div className="inner-content">
                     <div className="brand">
                         <Link to="/">
-                            <button className="headerBut">Homepage</button>
+                            <button className="headerBut">  Homepage  </button>
                         </Link>
                     </div>
-
+                    <div className="divider"></div>
                     <ul className="nav-links">
-
-                        <li>
-                            <Link to="/scoreboard">
-                                <button className="headerBut">Scoreboard</button>
-                            </Link>
-                        </li>
-
                         <li>
                             <Link to="/myscores">
-                                <button className="headerBut">My Scores</button>
+                                <button className="headerBut">  My Scores  </button>
                             </Link>
                         </li>
-
+                        <div className="divider"></div>
+                        <li>
+                            <Link to="/scoreboard">
+                                <button className="headerBut">  Scoreboard  </button>
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
