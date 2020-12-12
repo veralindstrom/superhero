b92c9e648948c7c41d16d2e45b3c6726171d4483
estelleh <estelleh@kth.se>
2020-12-13 00:36:36 +0100

    FakeAnswers updated

--- src/js/FakeQuizAnswers.js
+++ src/js/FakeQuizAnswers.js
@@ -1,17 +1,21 @@
 
 const fakeAnswers = {
-    0: ["Hammer smasher", "Soft Paw", "Insider Malone", "Queen of Kings", "Man of Sanddust"],
+    0: ["Hammer smasher", "Soft Paw", "Insider Malone", "Queen of Kings", "Man of Sanddust", "She-man", "The Thunder Lion", "Ziggy Stardust", "Wakame", "Mikadu", "Lightning Stone", "White Camealeon"],
     1: ["Arno Stark(father), Arnold(cousin), Edward Stark(uncle), May(aunt)", "Batwoman (syster), Harley(cousin), Catwoman(wife)", "Robin(father), Regina(mother), Serafina(aunt), Idina(daughter)", 
-            "Tantuli(uncle), Lexalet(father), Lanelo(adoptive mother)", "Little Mermaid(daughter), Poseidon(grandfather), Aquaman(cousin)"],
-    2: ["Heterochromia", "Berry violet", "Hazel-green", "Golden-amber", "Multicolored chromatic"],
-    3: ["Heroic", "Lawful good", "Chaotic", "Destructive", "Terrorist"],
-    4: ["Earis Dwarf", "Heale Angel", "Pixie Fairy", "Azerothian Deamon", "Hyborian Shapeshifter"],
-    5: ["Azors Hellfires", "Cave in the Sandy dunes", "Taxi backseat", "Prison cell", "Olympia Heaven"],
-    6: ["Bankman, Manager, CEO of Smoak Co.", "Taxi driver, Limo Driver, Buss driver", "Kindergarden teacher, Babysitter, Care taker for elderly", "Nanny, Priestess, Government lawyer", "Former Imam, Stree cleaner, Military Sergeant Major"],
-    7: ["Church, White Riders of the Night, Crusador of Justice", "Freedom Hall, Justice Lanthern Watchers", "The order of the World Council, Guardians of the Galaxy", "The Hellfires on Earth United, Sinestro Corps, Famine Rein", "Justice League Collaborated, Egalite Fraternite Liberte Combatant"],
-    8: ["Princess Epiteth", "Golden Sworn Touch", "Pink Panther", "Rambaskal", "Cahkla Silverback"],
-    9: ["LightHouse DC Comics", "Red Wolfs Comics", "Blacklight Animation", "Hawks Animated", "Freedom Justistice Fighters Cartoons"],
-    10: ["Taylor Lautner", "Jennifer Aniston", "Bruce Lee", "Robert De Niro", "Evian Halo"]
+            "Tantuli(uncle), Lexalet(father), Lanelo(adoptive mother)", "Little Mermaid(daughter), Poseidon(grandfather), Aquaman(cousin)", "Prince Kumkva(father, deceased), Queen Nalahl(mother), Foroug(uncled)",
+            "Dian Perks(brother), Fatila(grandmother)", "Unala Kahn(syster), Houdan Kahn(half-brother)", "Enaf(cousin)", "Leanne(daughter)", "Trone-heir Adulan(brother), High Priestess Bellunares(mother), High King Arkadi(father)", "Taomil(adoptiv-father)"],
+    2: ["Heterochromia", "Berry violet", "Hazel-green", "Golden-amber", "Multicolored chromatic", "Creole iris", "Ebony", "Azure", "Nomad", "Emerald", "saffire", "Chocolate-brown black"],
+    3: ["heroic", "lawful", "chaotic", "destructive", "terrorist", "criminal", "heroic", "lawful", "chaotic", "destructive", "terrorist", "criminal"],
+    4: ["Earis Dwarf", "Heale Angel", "Pixie Fairy", "Azerothian Deamon", "Hyborian Shapeshifter", "Parakeetian", "Wenge Arian", "Cocqueleti Gnome", "Fulvuonesian", "Xanadu Warrior", "Niccassar", "Andromedian Elf"],
+    5: ["Azors Hellfires", "Cave in the Sandy dunes", "Taxi backseat", "Prison cell", "Olympia Heaven", "Sarcaoulinis", "Serenditi", "Dead planet", "The deep forest", "Glacious wall of Smeargaf", "Cavern of Pluto", "Nebular Asteroid"],
+    6: ["Bankman, Manager, CEO of Smoak Co.", "Taxi driver, Limo Driver, Buss driver", "Kindergarden teacher, Babysitter, Care taker for elderly", "Nanny, Priestess, Government lawyer", "Former Imam, Stree cleaner, Military Sergeant Major", 
+            "Former town Major, Governor, Diplomat", "Garbage Truck driver, Scolar, Munk of tibet", "Astrofysicist, Pilot, Mercenary", "Winetaster, Chef, Assasin", "Butler, Cleaner, Investigator", "Professor, Archeologist, Historian", "FabricEmployee, Metalworker, Carver"],
+    7: ["Church, White Riders of the Night, Crusador of Justice", "Freedom Hall, Justice Lanthern Watchers", "The order of the World Council, Guardians of the Galaxy", "The Hellfires on Earth United, Sinestro Corps, Famine Rein", "Justice League Collaborated, Egalite Fraternite Liberte Combatant", 
+            "Freedom Peace fighter, Light Guardian ALien alliance", "Green Cape, Mantel of sacrifice bearer, Divine order Chairmans club ", "Crusador of Justice, Freedom Hall, White Light Guider", "Justice League, Guardian of absolute arder, Keepers of Secrets association", 
+            "Assasins Credentials Unit, Secret chaos services, Sinister undergroup group member", "Divine liberty, Lightbearer", "Equality for All United, Freedom Peace fighter"],
+    8: ["Princess Epiteth", "Golden Sworn Touch", "Pink Panther", "Rambaskal", "Cahkla Silverback", "Afknset Fea", "Willy Wally", "Amaranth", "Byza Tinen Allehl", "Prince Mauve Tanger", "Prince Eleaobarth Lissk", "High Priestess of the high skies"],
+    9: ["LightHouse DC Comics", "Red Wolfs Comics", "Blacklight Animation", "Hawks Animated", "Freedom Justistice Fighters Cartoons", "Pixar Light", "Blue Horn Comics", "Gray Wolf Animated", "Hero Leagues comics", "Heroics Animatited Catoons", "Heroes and Villans combats", "The Villains of DC comics"],
+    10: ["Tay Lautner", "Jennifer Austin", "Briane Lee", "Robert Everett", "Evian Halo", "Dinah Berrara", "Wilson Cain", "Payne Flawstawn", "Mika Amaritis", "Sammi Hya", "Tora Holt", "Andrea Madrox"]
 };
 
 export default fakeAnswers;
